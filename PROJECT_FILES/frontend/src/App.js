151ebcb36e4ca8f2aa8dbd3f93986c94cd129b93
ReshmaBanu_23BDS1170 <reshmabanu.a2023@vitstudent.ac.in>
2025-06-19 00:43:34 +0530

    Add files via upload

---
+++ PROJECT_FILES/frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import StudentDashboard from "./components/StudentDashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
