066cfc4ff5aaad4fd44f72c2bb91e46136bd79b6
ReshmaBanu_23BDS1170 <reshmabanu.a2023@vitstudent.ac.in>
2025-06-19 00:48:50 +0530

    Create security.py

---
+++ PROJECT_FILES/backend/app/core/security.py
@@ -0,0 +1,19 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "your_secret_key" 
+ALGORITHM = "HS256"
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(days=1)):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
